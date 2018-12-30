42fb6ee56f9975c919ebf789446d83465e669f01
Grzegorz Gebala <grzegorz.gebala@yahoo.com>
2018-12-30 11:06:18 +0100

    work commit

---
+++ script.js
@@ -0,0 +1,15 @@
+
+
+
+class Stopwatch extends React.Component {
+    render() {
+          return (
+              <div>abc</div>
+          )
+    }
+}
+
+ReactDOM.render(
+    <Stopwatch />,
+    document.getElementById('root')
+);
